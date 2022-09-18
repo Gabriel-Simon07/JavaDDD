0d6c72cbb2087239129813ecfb87f6ff71d75c6d
Gabriel Simon <gabrielsimon775@gmail.com>
2022-09-17 21:59:33 -0300

    changed name packages

---
+++ src/main/java/br/com/alura/escola/gamificacao/dominio/selo/RepositorioSelo.java
@@ -0,0 +1,11 @@
+package br.com.alura.escola.gamificacao.dominio.selo;
+
+import java.util.List;
+
+import br.com.alura.escola.academico.dominio.aluno.CPF;
+
+public interface RepositorioSelo {
+
+	void adicionar(Selo selo);
+	List<Selo> selosAlunoCpf(CPF cpf);
+}
---
+++ src/main/java/br/com/alura/escola/gamificacao/dominio/selo/Selo.java
@@ -0,0 +1,22 @@
+package br.com.alura.escola.gamificacao.dominio.selo;
+
+import br.com.alura.escola.academico.dominio.aluno.CPF;
+
+public class Selo {
+
+	private String nome;
+	private CPF cpf;
+
+	public Selo(String nome, CPF cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public CPF getCpf() {
+		return cpf;
+	}
+}
